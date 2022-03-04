1afdafb81d0eff66de7ed5b3ac667c04b15e69d5
zamanisima <zamanisima.uk@gmail.com>
2022-03-04 18:53:05 +0000

    created the main parts of the HTML

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
-function setup() {
+const rootTag = document.querySelector("#root");
+function gettingData() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  if (allEpisodes.length > 0) {
+    return render(allEpisodes);
+  } else {
+    return alert("Error from the server!");
+  }
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+const render = (allEpisodesShows) => {
+  //=======================================Header=====================
+  const headerTag = document.createElement("header");
+  headerTag.className = "header";
+  rootTag.appendChild(headerTag);
 
-window.onload = setup;
+  //========================================Main=====================
+  const mainTag = document.createElement("main");
+  mainTag.className = "main";
+  mainTag.innerHTML = `<h1>Found ${allEpisodesShows.length} Episode(s)</h1>`;
+  rootTag.appendChild(mainTag);
+
+  //=========================================Footer===================
+  const footerTag = document.createElement("footer");
+  footerTag.className = "footer";
+  rootTag.appendChild(footerTag);
+};
+window.onload = gettingData;
