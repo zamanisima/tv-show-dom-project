34ef6c783f1ff027da7e8959a2eb439402f45808
zamanisima <zamanisima.uk@gmail.com>
2022-03-05 18:17:44 +0000

    the incase senstive and filter serach is working

--- script.js
+++ script.js
@@ -8,48 +8,15 @@ function gettingData() {
     return alert("Error from the server!");
   }
 }
-//========================================Rendering the webPage===========
-const render = (allEpisodesShows) => {
-  console.log(allEpisodesShows);
-  //=================================Creating episode title=======
-  const createTitle = (name, season, num) => {
-    return `S${String(season).padStart(2, 0)}E${String(num).padStart(
-      2,
-      0
-    )} - ${name}`;
-  };
-  //================================Header=====================
-  const headerTag = document.createElement("header");
-  headerTag.className = "header";
-  rootTag.appendChild(headerTag);
+const createTitle = (name, season, num) => {
+  return `S${String(season).padStart(2, 0)}E${String(num).padStart(
+    2,
+    0
+  )} - ${name}`;
+};
 
-  //========================================Main=====================
-  const mainTag = document.createElement("main");
-  mainTag.className = "main";
-  mainTag.innerHTML = `<h1>Found ${allEpisodesShows.length} Episode(s)</h1>`;
-  rootTag.appendChild(mainTag);
-
-  //============================searchBar=============================
-  let allEpisodeTitle = [];
-
-  const searchBar = document.createElement("input");
-  searchBar.type = "text";
-  mainTag.appendChild(searchBar);
-  console.log(searchBar);
-
-  searchBar.addEventListener("keyup", (e) => {
-    const searchString = e.target.value.toLowerCase();
-    console.log(searchString);
-    const filteredEpisodes = allEpisodeTitle.filter((episode) => {
-      episode.name.toLowerCase().includes(searchString) ||
-        season.name.toLowerCase().includes(searchString);
-    });
-    console.log(filteredEpisodes);
-  });
-
-  //================================Episodes Article=============
-  const articleTag = document.createElement("article");
-  articleTag.className = "episodes_article";
+function renderEpisode(allEpisodesShows, articleTag) {
+  articleTag.innerHTML = "";
 
   allEpisodesShows.forEach((item) => {
     const episode = document.createElement("div");
@@ -66,7 +33,51 @@ const render = (allEpisodesShows) => {
 
     articleTag.appendChild(episode);
   });
+}
+
+//========================================Rendering the webPage===========
+const render = (allEpisodesShows) => {
+  console.log(allEpisodesShows);
+
+  //================================Header=====================
+  const headerTag = document.createElement("header");
+  headerTag.className = "header";
+  rootTag.appendChild(headerTag);
+
+  //========================================Main=====================
+  const mainTag = document.createElement("main");
+  mainTag.className = "main";
+  mainTag.innerHTML = `<h1>Found ${allEpisodesShows.length} Episode(s)</h1>`;
+  rootTag.appendChild(mainTag);
+
+  //============================searchBar=============================
+
+  const searchBar = document.createElement("input");
+  searchBar.type = "text";
+  mainTag.appendChild(searchBar);
+  console.log(searchBar);
+
+  searchBar.addEventListener("keyup", (e) => {
+    const searchString = e.target.value.toLowerCase();
+    console.log(searchString);
+    const filteredEpisodes = allEpisodesShows.filter((episode) => {
+      return episode.name.toLowerCase().includes(searchString);
+      //||
+      //season.name.toLowerCase().includes(searchString)
+    });
+
+    //const filteredEpisode =
+    console.log(filteredEpisodes);
+    renderEpisode(filteredEpisodes, articleTag);
+  });
+
+  //================================Episodes Article=============
+
+  const articleTag = document.createElement("article");
+  articleTag.className = "episodes_article";
+  renderEpisode(allEpisodesShows, articleTag);
   mainTag.appendChild(articleTag);
+
   //=========================================Footer===================
   const footerTag = document.createElement("footer");
   footerTag.className = "footer";
